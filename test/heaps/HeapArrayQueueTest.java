50f9ee403992e06a6fb951a0d1bf95660fe9fee6
TortillaDePatat4 <tortilladepatat4@gmail.com>
2025-11-29 11:46:36 +0100

    commit V2 + Tests

--- test/heaps/HeapArrayQueueTest.java
+++ test/heaps/HeapArrayQueueTest.java
@@ -1,7 +1,58 @@
 package heaps;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class HeapArrayQueueTest {
+public class HeapArrayQueueTest {
 
+    @Test
+    void testInitialSizeIsZero() {
+        HeapArrayQueue<Integer, String> q = new HeapArrayQueue<>();
+        assertEquals(0, q.size());
+    }
+
+    @Test
+    void testAddSingleElement() {
+        HeapArrayQueue<Integer, String> q = new HeapArrayQueue<>();
+        q.add(10, "A");
+        assertEquals(1, q.size());
+        assertEquals("A", q.element());
+    }
+
+    @Test
+    void testAddMaintainsHeapProperty() {
+        HeapArrayQueue<Integer, String> q = new HeapArrayQueue<>();
+        q.add(1, "A");
+        q.add(10, "B");
+        q.add(5, "C");
+
+        assertEquals("B", q.element());
+    }
+
+    @Test void testRemoveReturnsInCorrectOrder() {
+        HeapArrayQueue<Integer, String> q = new HeapArrayQueue<>();
+        q.add(3, "A"); q.add(10, "B");
+        q.add(7, "C"); assertEquals("B", q.remove());
+        assertEquals("C", q.remove()); assertEquals("A", q.remove());
+        assertEquals(0, q.size());
+    }
+
+    @Test void testStableOrderWithSamePriority() {
+        HeapArrayQueue<Integer, String> q = new HeapArrayQueue<>();
+        q.add(5, "A");
+        assertEquals("A", q.remove());
+        assertEquals("B", q.remove());
+        assertEquals("C", q.remove());
+    }
+
+    @Test void testCapacityExpansion() {
+        HeapArrayQueue<Integer, Integer> q = new HeapArrayQueue<>();
+
+        for (int i = 0; i < 20; i++) {
+            q.add(i, i);
+        }
+
+        assertEquals(20, q.size());
+    }
 }
---
+++ test/heaps/TripletsTest.java
@@ -0,0 +1,30 @@
+package heaps;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TripletsTest {
+
+    @Test
+    void testSomeCompare() {
+        var triplet1 = new HeapArrayQueue.Triplet<Integer, Object>(
+                1, 1L, new Object());
+        var triplet2 = new HeapArrayQueue.Triplet<Integer, Object>(
+                2, 2L, new Object());
+        assertTrue(triplet2.compareTo(triplet1) > 0);
+    }
+
+    @Test void testPriorityComparison() {
+        var t1 = new HeapArrayQueue.Triplet<>(1, 1L, "A");
+        var t2 = new HeapArrayQueue.Triplet<>(2, 2L, "B");
+        assertTrue(t2.compareTo(t1) > 0);
+        assertTrue(t1.compareTo(t2) < 0);
+    }
+
+    @Test void testEqualPriorityUsesTimestamp() {
+        var t1 = new HeapArrayQueue.Triplet<>(5, 1L, "A");
+        var t2 = new HeapArrayQueue.Triplet<>(5, 2L, "B");
+        assertTrue(t2.compareTo(t1) > 0);
+    }
+}
